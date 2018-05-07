80f3a938e566d4484e6da99dc255ac067deb5d28
alieeeeeeeen <elaienchen77@icloud.com>
2018-05-07 13:54:09 +0800

    hoc

--- src/higer-order-component/passData.jsx
+++ src/higer-order-component/passData.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 // pure function
 
 function passData(WrappedComponent, fetchData) {
-
   return class extends React.Component {
     constructor(props) {
       super(props);
